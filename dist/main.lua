029709edad47978c3ad81ee9b44a9d69491f6050
FakeModz <78726008+FakeModz@users.noreply.github.com>
2025-07-25 04:52:15 +0700

    Update main.lua

--- dist/main.lua
+++ dist/main.lua
@@ -6071,7 +6071,7 @@ AutomaticSize="XY",
 BackgroundTransparency=1,
 },{
 b("TextLabel",{
-Text=o.User.Anonymous and"Anonymous"or game.Players.LocalPlayer.DisplayName,
+Text=o.User.Anonymous and"LimitHub"or game.Players.LocalPlayer.DisplayName,
 TextSize=17,
 ThemeTag={
 TextColor3="Text",
@@ -6084,7 +6084,7 @@ TextTruncate="AtEnd",
 TextXAlignment="Left",
 }),
 b("TextLabel",{
-Text=o.User.Anonymous and"@anonymous"or"@"..game.Players.LocalPlayer.Name,
+Text=o.User.Anonymous and"@LimitHub"or"@"..game.Players.LocalPlayer.Name,
 TextSize=15,
 TextTransparency=.6,
 ThemeTag={
